b5e57cd64aa0e5fc95ae07acc748a336c0dcbbf9
mohit <mohit.gyanchandani09@gmail.com>
2025-12-19 07:50:38 +0530

    test

---
+++ css/style.cc
@@ -0,0 +1,15 @@
+body {
+  font-family: system-ui, sans-serif;
+  max-width: 700px;
+  margin: 40px auto;
+  line-height: 1.6;
+}
+
+a {
+  color: #0066cc;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
